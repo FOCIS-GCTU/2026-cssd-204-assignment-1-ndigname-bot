4f69a1ad6e982d913d65b9c6271cdabc778f6d23
Emmanuel Ndignam <ndigname@gmail.com>
2026-05-27 21:05:06 +0000

    scripting language assignment 1
    Added detailed comments and structure to the main function for printing initials. this is our practical assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,48 @@
 
-def main():
-  pass #code goes here
+    def main():
 
+   # File: initials.py
+    # Description: Print my initials ENN in stylized large letters
+    # Assignment Number: 1
+    #
+    # Name: Emmanuel Ndignam Nelimo
+    # STUDENT ID: 2425401719
+    # Email: ndigname@gmail.com
+    # Grader: Carolyn
 
+    def main():
+        # This program prints initials E, N, N as large block letters
+        # Each letter is 12 characters wide by 10 lines high
+        # Each large letter is made of its own character (E or N)
+        # Three periods on left and right of each letter/period pair
 
-main()
+        # Small initials line with blank lines
+        print()
+        print("...ENN")
+        print()
+
+        # Line 1
+        print("...EEEEEEEEEEEE..........NN.......................NN.........NN.......................NN " )
+        # Line 2
+        print("...E.....................NN ..NN .................NN.........NN ..NN .................NN ")
+        # Line 3
+        print("...E.....................NN......NN...............NN.........NN......NN...............NN")
+        # Line 4
+        print("...EEEEEEEEEEEE..........NN.........NN ...........NN.........NN.........NN ...........NN.")
+        # Line 5
+        print("...E.....................NN... .......NN..........NN.........NN... .......NN..........NN.")
+        # Line 6
+        print("...E.....................NN .. .........NN........NN.........NN .. .........NN........NN")
+        # Line 7
+        print("...E.....................NN ..............NN......NN.........NN ..............NN......NN.")
+        # Line 8
+        print("...E..............****...NN.................NN....NN...***...NN.................NN....NN.")
+        # Line 9
+        print("...E           ...****...NN...................NN..NN...***...NN...................NN..NN  ")
+        # Line 10
+        print("...EEEEEEEEEEEE...****...NN.....................NNNN...***...NN.....................NNNN        NN****")
+
+        print()
+
+    main()
+
