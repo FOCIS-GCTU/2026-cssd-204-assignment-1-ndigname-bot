b4aa1f87d9473dba22dfd336c27ac51cdaf4d2e1
Emmanuel Ndignam <ndigname@gmail.com>
2026-05-27 21:15:35 +0000

    Clean up initials.py and streamline main function
    Refactor code to improve readability and structure.

--- initials.py
+++ initials.py
@@ -1,48 +1,48 @@
+# File: initials.py
+# Description: Print my initials ENN in stylized large letters
+# Assignment Number: 1
+#
+# Name: Emmanuel Ndignam Nelimo
+# STUDENT ID: 2425401719
+# Email: ndigname@gmail.com
+# Grader: Carolyn
+#
+# On my honor, Emmanuel Ndignam Nelimo, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-    def main():
 
-   # File: initials.py
-    # Description: Print my initials ENN in stylized large letters
-    # Assignment Number: 1
-    #
-    # Name: Emmanuel Ndignam Nelimo
-    # STUDENT ID: 2425401719
-    # Email: ndigname@gmail.com
-    # Grader: Carolyn
 
-    def main():
-        # This program prints initials E, N, N as large block letters
-        # Each letter is 12 characters wide by 10 lines high
-        # Each large letter is made of its own character (E or N)
-        # Three periods on left and right of each letter/period pair
+def main():
+    # This program prints initials E, N, N as large block letters
+    # Each letter is 12 characters wide by 10 lines high
+    # Each large letter is made of its own character (E or N)
+    # Three periods on left and right of each letter/period pair
+    
+    print()
+    print("...ENN...")
+    print()
+    
+     # Line 1
+    print("...EEEEEEEEEEEE****...NN        NN****...NN        NN****")
+    # Line 2
+    print("...E           ****...NNN      NNN****...NNN      NNN****")
+    # Line 3
+    print("...E           ****...NNNN    NNNN****...NNNN    NNNN****")
+    # Line 4
+    print("...EEEEEEEEEEEE****...NNNNN  NNNNN****...NNNNN  NNNNN****")
+    # Line 5
+    print("...E           ****...NNNNNNNNNNNN****...NNNNNNNNNNNN****")
+    # Line 6
+    print("...E           ****...NNNNNNNNNNNN****...NNNNNNNNNNNN****")
+    # Line 7
+    print("...E           ****...NNNNN  NNNNN****...NNNNN  NNNNN****")
+    # Line 8
+    print("...E           ****...NNNN    NNNN****...NNNN    NNNN****")
+    # Line 9
+    print("...E           ****...NNN      NNN****...NNN      NNN****")
+    # Line 10
+    print("...EEEEEEEEEEEE****...NN        NN****...NN        NN****")
+    
+    print()
 
-        # Small initials line with blank lines
-        print()
-        print("...ENN")
-        print()
-
-        # Line 1
-        print("...EEEEEEEEEEEE..........NN.......................NN.........NN.......................NN " )
-        # Line 2
-        print("...E.....................NN ..NN .................NN.........NN ..NN .................NN ")
-        # Line 3
-        print("...E.....................NN......NN...............NN.........NN......NN...............NN")
-        # Line 4
-        print("...EEEEEEEEEEEE..........NN.........NN ...........NN.........NN.........NN ...........NN.")
-        # Line 5
-        print("...E.....................NN... .......NN..........NN.........NN... .......NN..........NN.")
-        # Line 6
-        print("...E.....................NN .. .........NN........NN.........NN .. .........NN........NN")
-        # Line 7
-        print("...E.....................NN ..............NN......NN.........NN ..............NN......NN.")
-        # Line 8
-        print("...E..............****...NN.................NN....NN...***...NN.................NN....NN.")
-        # Line 9
-        print("...E           ...****...NN...................NN..NN...***...NN...................NN..NN  ")
-        # Line 10
-        print("...EEEEEEEEEEEE...****...NN.....................NNNN...***...NN.....................NNNN        NN****")
-
-        print()
-
-    main()
-
+main()
